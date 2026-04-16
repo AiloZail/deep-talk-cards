8a03110abba74abd97a349d0266f7c760d47a594
AiloZail <natapongkeng@gmail.com>
2026-04-16 15:49:01 +0700

    Initial commit: setup project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-purple-500 to-pink-500 flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-white">
+        Deep Talk Cards
+      </h1>
+    </div>
+  )
+}
+
+export default App
